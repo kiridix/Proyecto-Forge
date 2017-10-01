4a6606fb110e6788c145bfdf1b6d8961a26cfc71
kiridix <matimar1414@gmail.com>
2017-10-01 16:25:20 -0300

    Connexion con la base de datos

---
+++ ProyectoForge/BaseDeDatos.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProbandoConeccionSQLServer
+{
+    public class BaseDeDatos
+    {
+
+
+        private static SqlConnection conn = new SqlConnection("user id=dev;password=forge2017;server=devforge.c0muduoou4ac.us-east-1.rds.amazonaws.com;Trusted_Connection=No;database=pruebaxd;connection timeout=30");
+        private static SqlCommand cmnd;
+        private static SqlDataReader dtr;
+
+        public static Boolean TestConnection()
+        {
+            try
+            {
+                conn.Open();
+                conn.Close();
+                return (true);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.ToString());
+                return (false);
+            }
+        }
+        public static int InsertarPersona(int ci, string nombre, string apellido)
+        {
+            try
+            {
+                conn.Open();
+                String sentenciaSQL = "INSERT INTO persona VALUES (" + ci + ", '" + nombre + "', '" + apellido + "')";
+                cmnd = new SqlCommand(sentenciaSQL, conn);
+                cmnd.ExecuteNonQuery();
+                conn.Close();
+                return 0;
+            }
+            catch (Exception e)
+            {
+                switch (e.HResult)
+                {
+                    case -2146232060:
+                        conn.Close();
+                        return 1;
+                    default:
+                        MessageBox.Show(e.ToString() + "\n\n" + "HResult: " + e.HResult.ToString());
+                        conn.Close();
+                        return e.HResult;
+                }
+            }
+        }
+
+        public static System.Data.DataTable ListPeople()
+        {
+            System.Data.DataTable dt = new System.Data.DataTable();
+            try
+            {
+                conn.Open();
+                cmnd = new SqlCommand("SELECT * FROM persona", conn);
+                dtr = cmnd.ExecuteReader();
+                dt.Load(dtr);
+                dtr.Close();
+                conn.Close();
+                return dt;
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
+        }
+    }
+}
